5d5da9d49df0abf4c57236d46ab0d63843ccf455
claudio dantas <claudiodantas16@hotmail.com>
2021-08-08 22:24:33 -0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import time
-import random
 from datetime import datetime
-
-random_time = random.randint(3600, 18000)
-rand_time = round((random_time/3600), 2)
 
 from parking_lot import ParkingLot
 from client import Client
@@ -17,9 +13,7 @@ client_3 = Client('Rafael Nepomuceno', 'RIO2A98')
 
 ufop_parking.greetings()
 spot = ufop_parking.check_vacancy()
-
 time.sleep(2)
-
 if spot == 0:
     print('Nao ha vagas disponiveis no momento!')
 else:
@@ -29,8 +23,26 @@ else:
     time.sleep(1)
     ticket = ufop_parking.generate_ticket(start, car_spot)
     print('Ticket gerado...')
-    time.sleep(10)
+    time.sleep(2)
     ufop_parking.store_costumer_info(client_1.plate_number, car_spot)
+    ufop_parking.unpark_car(car_spot)
+    end = datetime.now()
+    ufop_parking.bill_generate(start, end)
+    
+    
+spot = ufop_parking.check_vacancy()
+time.sleep(2)
+if spot == 0:
+    print('Nao ha vagas disponiveis no momento!')
+else:
+    start = datetime.now()
+    car_spot = ufop_parking.park_car(client_2.plate_number)
+    print('Gerando ticket...')
+    time.sleep(1)
+    ticket = ufop_parking.generate_ticket(start, car_spot)
+    print('Ticket gerado...')
+    time.sleep(2)
+    ufop_parking.store_costumer_info(client_2.plate_number, car_spot)
     ufop_parking.unpark_car(car_spot)
     end = datetime.now()
     ufop_parking.bill_generate(start, end)
